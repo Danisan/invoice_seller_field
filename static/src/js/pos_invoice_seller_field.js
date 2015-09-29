b8d45d3e11479fdd2e9a918e39bf4163a8354f7a
ufaks <excellm@yandex.ru>
2015-09-29 20:16:47 +0500

    [IMP] Add seller in invoice

--- static/src/js/pos_invoice_seller_field.js
+++ static/src/js/pos_invoice_seller_field.js
@@ -11,6 +11,9 @@ openerp.invoice_seller_field = function(instance){
             model.call("get_sellers", {context: new instance.web.CompoundContext()}).then(function(result) {
                 self.$el.append(QWeb.render('HeaderSellerField', {sellers: result}));
             });
+            $('.header-seller').on('change', '.sellers_select', function(){
+                self.pos.get('selectedOrder').set_seller(this.value);
+            });
         },
     });
 
@@ -21,4 +24,26 @@ openerp.invoice_seller_field = function(instance){
             this.headerseller.insertAfter(this.$('.pos-logo'));
         },
     });
+
+    var ModuleOrderSuper = module.Order;
+    module.Order = module.Order.extend({
+        initialize: function(options){
+            ModuleOrderSuper.prototype.initialize.apply(this, arguments);
+            this.set({
+                seller: '00',
+            });
+        },
+        export_as_JSON: function () {
+            var res = ModuleOrderSuper.prototype.export_as_JSON.apply(this, arguments);
+            res.seller_id = this.get_seller();
+            return res;
+        },
+        set_seller: function(seller){
+            this.set('seller', seller);
+        },
+        get_seller: function(){
+            return this.get('seller');
+        },
+    });
+
 }
